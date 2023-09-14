39c3862c579b54816d8228cf4586ab8d74a744f3
michael mbugua <mikembugua960@gmail.com>
2023-09-14 05:43:30 +0300

    different data types  in php

---
+++ variables.php
@@ -0,0 +1,24 @@
+<?php
+echo "mike";
+
+// we have the following data types in php
+/*integer
+float
+boolean
+strings
+decimal
+arrays
+objects
+resource
+
+Variavble rules
+ they must start with a $
+ you can only start with an underscore or alpha-numeric characters
+ they are case sensitive
+
+*/
+
+$name = "Michael";
+$age = 22;
+var_dump($name);
+var_dump($age);
